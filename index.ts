47bbe12c374dc5eafb4923dd48912e41d5a7d552
Ivan Prodaiko <i.prodaiko@nodeart.io>
2017-07-08 16:38:25 +0300

    - exports

--- index.ts
+++ index.ts
@@ -1,9 +1,6 @@
-// export { SortableTableComponent } from './src/components/sortable_table/sortable-table.component';
-// export { LoadingComponent } from './src/components/loading/loading.component';
-// export { DialogDirective } from './src/directives/dialog.directive';
-// export { InfiniteScrollDirective } from './src/directives/infinite-scroll.directive';
-// export { PriorityKeysPipe } from './src/pipes/priority-keys.pipe';
-// export { SortableTableService } from './src/services/sortable-table.service';
+export { SortableEvents, TableConfig, PathConfig } from './src/models/';
 
-// export * from './src/models/';
+export { NgFbSortableTableComponent } from './src/components/ngfb-sortable-table/ngfb-sortable-table.component';
+export { NgFbSortableTableService } from './src/services/sortable-table.service';
+
 export { NgFbSortableTableModule } from './src/ngfb-sortable-table.module';
